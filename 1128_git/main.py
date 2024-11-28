6e22bf18f33af6399189a7abf2b7acb63bc9c6b1
geo_prepod <maksikbet13@gmail.com>
2024-11-28 16:42:08 +0300

    github loading 2

--- 1128_git/main.py
+++ 1128_git/main.py
@@ -2,6 +2,7 @@ def main():
     print("My first git programm!")
     print("And I complete changes every day")
     # added comment
+    print("Still a bit information")
 
 if __name__ == '__main__':
     main()
